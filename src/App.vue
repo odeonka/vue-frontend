9b68ac6d99686bccef976a65d45f0cc3690f0f78
odeonka <66359179+odeonka@users.noreply.github.com>
2020-11-11 15:36:53 +0700

    vue-frontend-can

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <div class="container pt-4">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+  import Navbar from '@/components/Navbar.vue'
+
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  }
+  #app .navbar-nav a{
+  color: #2c3e50;
+  cursor: pointer;
+  }
+  #app .navbar-nav a.router-link-exact-active {
+  color: #42b983;
+  }
+</style>
